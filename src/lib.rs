97a5303f543a4475a93bdf98095f5e5194b7e1a8
FlareFlo <mail@flareflo.dev>
2022-09-28 20:38:05 +0200

    Directly read path to manifest

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
 use proc_macro::{TokenStream};
 use quote::quote;
-use std::fs;
 use cargo_toml::Manifest;
 
 
 #[proc_macro]
 pub fn package_name(_input: TokenStream) -> TokenStream {
     // let parsed = parse_macro_input!(input as DeriveInput);
-    let toml = fs::read("Cargo.toml").unwrap();
-
-    let manifest = Manifest::from_slice(&toml).unwrap();
+    let manifest = Manifest::from_path("Cargo.toml").unwrap();
 
     let name = manifest.package.unwrap().name;
 
